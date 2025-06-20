c309a154c837895d7087393dccded378e8f03fc6
Hadi Youssef <hmyoussef@gmail.com>
2025-06-19 23:01:29 -0400

    Fixed docstrings of practice tools

--- tools/calculator.py
+++ tools/calculator.py
@@ -5,22 +5,59 @@ def register_tools(mcp: FastMCP):
     
     @mcp.tool()
     def add(a: float, b: float) -> float:
-        """Add two numbers."""
+        """
+        Add two numbers.
+        
+        Args:
+            a: The first number.
+            b: The second number.
+
+        Returns:
+            The sum of the two numbers.
+        """
         return a + b
 
     @mcp.tool()
     def subtract(a: float, b: float) -> float:
-        """Subtract two numbers."""
+        """
+        Subtract two numbers.
+
+        Args:
+            a: The first number to be subtracted
+            b: The second number to be subtracted
+        
+            Returns:
+                The difference of the two numbers
+        
+        """
         return a - b
 
     @mcp.tool()
     def multiply(a: float, b: float) -> float:
-        """Multiply two numbers."""
+        """
+        Multiply two numbers
+        
+        Args: 
+            a: The first number to be multiplied
+            b: The second number to be multiplied
+
+        Returns:
+            The product of the two numbers
+        """
         return a * b
 
     @mcp.tool()
     def divide(a: float, b: float) -> float:
-        """Divide two numbers."""
+        """
+        Divide two numbers.
+        
+        Args:
+            a: The numerator
+            b: The denominator
+        
+        Returns:
+            The quotient of the two numbers        
+        """
         if b == 0:
             raise ValueError("Cannot divide by zero.")
         return a / b
--- tools/echo.py
+++ tools/echo.py
@@ -4,5 +4,12 @@ def register_tools(mcp: FastMCP):
     """Register tools with the MCP server."""
     @mcp.tool()
     def echo_tool(message: str) -> str:
-        """Echo a message"""
+        """Echo a message
+        
+        Args: 
+            message: The message you want echo'd
+        
+        Returns:
+            The echo'd message
+        """
         return f"Echo: {message}"
